d64a1cbc135c846d1e9cbd5f16442fc2dda3665d
ZRMYDYCG <547471919@qq.com>
2025-03-04 11:50:01 +0800

    feat: ✨️简单初始化axios配置,携带token

---
+++ src/settings/config/http.ts
@@ -0,0 +1,16 @@
+// 请求地址
+export const HTTP_HOST = 'http://localhost:3000'
+
+// 请求超时时长
+export const HTTP_TIMEOUT = 5000
+
+// 业务处理成功状态码
+export const HTTP_SUCCESS_CODE = 200
+
+// 登录状态失效状态码
+export const HTTP_TOKEN_ERROR_CODE = 400
+
+export const HTTP_CODE = {
+  HTTP_SUCCESS_CODE,
+  HTTP_TOKEN_ERROR_CODE,
+}
---
+++ src/utils/http/index.ts
@@ -0,0 +1,33 @@
+import { HTTP_TIMEOUT } from '@/settings/config/http'
+import useUserStore from '@/store/modules/user'
+import axios from 'axios'
+
+// 创建http axios实例
+const http = axios.create({
+  // baseURL: HTTP_HOST,
+  timeout: HTTP_TIMEOUT,
+})
+
+// 请求拦截
+http.interceptors.request.use(
+  function (config) {
+    const userStore = useUserStore()
+    config.headers.Authorization = userStore.token
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+// 响应拦截
+http.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+export default http
