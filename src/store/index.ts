b28991d22f4de4bcc9458a18d101199b4616de3c
ZRMYDYCG <547471919@qq.com>
2025-03-06 17:38:33 +0800

    fix: 🪲️提取缓存token回存store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,20 @@
+import { useUserStore } from '@/store/modules/user.ts'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 
 export const registerStore = (app) => {
   app.use(pinia)
+
+  const userStore = useUserStore()
+  const user = localStorage.getItem('user')
+
+  if (user) {
+    userStore.updateUserInfo(JSON.parse(user))
+  }
+
+  const token = localStorage.getItem('token')
+  if (token) {
+    userStore.updateToken(token)
+  }
 }
